06dc17c26e349715b4b0b95c6978c35e89bb6612
Calegria27 <carlos.alegria@independencia.cl>
2023-01-05 16:47:40 -0300

    new route added

--- routes/user.py
+++ routes/user.py
@@ -1,8 +1,22 @@
 from fastapi import APIRouter
+from config.db import conn
+from sqlalchemy import text
 
 
 router=APIRouter()
 
-@router.get('/')
-async def get_info():
-    return "HolaMundo"
+
+
+@router.post('/')
+async def get_info(item:dict):
+    stmt=text("select Usu_Cuenta,Usu_Nombre,Perfil from INDMaeUsuario where Usu_Cuenta='"+item["Usu_Cuenta"]+"' and Usu_Password='"+item["Usu_Password"]+"'")
+    users=conn.execute(stmt)
+    usersList=users.fetchall()
+    return usersList
+
+@router.post('/user/empresas')
+async def get_empresas(item:str):
+    stmt=("SELECT maeEmpresa.empCodigo, maeEmpresa.empNombre FROM INDUsuNegocio INNER JOIN maeEmpresa ON INDUsuNegocio.CtoEmpresa = maeEmpresa.empCodigo WHERE (INDUsuNegocio.Usu_Cuenta ='"+item+"')group by maeEmpresa.empCodigo, maeEmpresa.empNombre")
+    empresas=conn.execute(stmt)
+    empresasList=empresas.fetchall()
+    return empresasList
