d7e6b9a584c0b9f1155b221c888a00eecada6008
Calegria27 <carlos.alegria@independencia.cl>
2023-06-12 14:44:35 -0400

    change

--- config/db.py
+++ config/db.py
@@ -4,5 +4,5 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Replace 'server' and 'database' with the name of your SQL Server instance and the name of your database
-engine = create_engine('mssql+pymssql://UsuSistratos:15678.usu@192.168.1.31:1433/SistratosWeb', pool_pre_ping=True)
+engine = create_engine('mssql+pymssql://UsuSistratos:15678.usu@192.168.1.44:1433/SistratosWeb', pool_pre_ping=True)
 conn= engine.connect()
