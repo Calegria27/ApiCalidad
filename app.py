99076bd179f8a3e46385da83ff46ca199aa21ede
Carlos <Carlos Alegria>
2023-06-30 11:02:12 -0400

    added

--- app.py
+++ app.py
@@ -6,22 +6,16 @@ from fastapi.middleware.cors import CORSMiddleware
 app=FastAPI()
 
 origins = [
-    "*"
-    # "http://190.82.118.130",
-    # "http://190.82.118.130:5174/",
-    # "http://190.82.118.130:5174/login",
-    # "http://190.82.118.130:5174/home",
-    # "http://sistratoscalidad.cindependencia.cl",
-    # "http://sistratoscalidad.cindependencia.cl/login",
-    # "http://sistratoscalidad.cindependencia.cl/home",
-    # "http://sistratoscalidad.cindependencia.cl:5174",
-    # "http://sistratoscalidad.cindependencia.cl:5174/login",
-    # "http://sistratoscalidad.cindependencia.cl:5174/home",
-    # "http://localhost",
-    # "http://localhost:5174",
-    # "http://192.168.1.16:5174",
-    # "http://192.168.1.16:5174/",
-    # "http://localhost:5174/home",
+    "http://190.82.118.130",
+    "http://190.82.118.130:5174/",
+    "http://190.82.118.130:5174/login",
+    "http://190.82.118.130:5174/home",
+    "http://sistratoscalidad.cindependencia.cl",
+    "http://sistratoscalidad.cindependencia.cl/login",
+    "http://sistratoscalidad.cindependencia.cl/home",
+    "http://sistratoscalidad.cindependencia.cl:5174",
+    "http://sistratoscalidad.cindependencia.cl:5174/login",
+    "http://sistratoscalidad.cindependencia.cl:5174/home",
 ]
 
 app.add_middleware(
